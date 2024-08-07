85b21a2d753ffe954909be3ea028ac5b79e19f7c
RavishCRZ27 <ravishpandey27@gmail.com>
2024-08-07 22:36:38 +0530

    week 2 complete

---
+++ 100xDevs/0-1/week2/map,filter.js
@@ -0,0 +1,33 @@
+// arrow, map, filter
+
+// function sum(a,b){
+//     return a+b
+// }
+// console.log(sum(3,5))
+// const a = (a,b) =>{
+//     return a+b;
+// }
+
+//given an array give me back an array in which every value is multiplied by 2
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(
+//   arr.map(function (i) {
+//     return i * 2;
+//   })
+// );
+
+
+//given an array, return an array with all even values
+
+// const arr= [1,2,3,4,5,6,7,8]
+// function filtering(n){
+//     if(n%2==0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// const ans = arr.filter(filtering)
+// console.log(ans)
